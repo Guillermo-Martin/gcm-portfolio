3e25c4cea1e4f8c94a55858f725001bec75b8e9d
gcmartin <gscalica@gmail.com>
2020-07-25 16:23:14 -0700

    Create 'Components' and 'Assets' folders and create HeroImage component and styles.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HeroImage from './components/HeroImage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello.</h1>
+      <HeroImage />
     </div>
   );
 }
---
+++ src/components/HeroImage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './../assets/styles/styles.css';
+
+function HeroImage() {
+  return (
+    <div>
+      {/* image from https://unsplash.com/.  Photo taken by Tim Trad (https://unsplash.com/@timtrad) */}
+      {/* <img 
+        src="https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"
+        alt="Lauterbrunnen"
+      /> */}
+      <div className="HeroImage-img">
+        <div className="HeroImage-text">
+          <h1>Lorem ipsum</h1>
+          <h2>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroImage;
