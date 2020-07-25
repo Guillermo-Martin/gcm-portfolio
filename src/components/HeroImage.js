4b1718ff12fa5037f68599cb028c8ad39577f643
gcmartin <gscalica@gmail.com>
2020-07-25 16:43:29 -0700

    Style hero image

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -5,15 +5,16 @@ function HeroImage() {
   return (
     <div>
       {/* image from https://unsplash.com/.  Photo taken by Tim Trad (https://unsplash.com/@timtrad) */}
-      {/* <img 
+    
+      <img 
+        className="HeroImage-img"
         src="https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"
         alt="Lauterbrunnen"
-      /> */}
-      <div className="HeroImage-img">
-        <div className="HeroImage-text">
-          <h1>Lorem ipsum</h1>
-          <h2>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h2>
-        </div>
+      />
+
+      <div className="HeroImage-text">
+        <h1>Lorem ipsum</h1>
+        <h2>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h2>
       </div>
     </div>
   );
