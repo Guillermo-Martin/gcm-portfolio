7a8dcf6b45dda96d1fa64bf222bb6a12fc82c252
gcmartin <gscalica@gmail.com>
2020-07-25 20:56:32 -0700

    Move hero image into styles.css and re-style hero image

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import './../assets/styles/styles.css';
 
 function HeroImage() {
   return (
-    <div className="HeroImage-container">
-      {/* image from https://unsplash.com/.  Photo taken by Tim Trad (https://unsplash.com/@timtrad) */}
-    
-      <img 
-        className="HeroImage-img"
-        src="https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"
-        alt="Lauterbrunnen"
-      />
 
-      <div className="HeroImage-text">
-        <h1 className="HeroImage-text-name">Lorem ipsum</h1>
-        <h2 className="HeroImage-text-intro">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h2>
+    
+      
+
+      <div className="HeroImage-img">
+        <div className="HeroImage-text">
+          <h1 className="HeroImage-text-name">Lorem ipsum</h1>
+          <h2 className="HeroImage-text-intro">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h2>
+          <h2></h2>
+        </div>
       </div>
-    </div>
+
+
+      
+
+
+    
+
+    // <div>
+    //   <div className="HeroImage-container">
+
+    //       <div className="HeroImage-text">
+    //         <h1 className="HeroImage-text-name">Lorem ipsum</h1>
+    //         <h2 className="HeroImage-text-intro">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h2>
+    //       </div>
+
+    //   </div>
+    // </div>
+
+
   );
 }
 
