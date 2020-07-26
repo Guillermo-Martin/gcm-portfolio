f6b34003bd2c57f638bf94ed59c42eae3d9b38ae
gcmartin <gscalica@gmail.com>
2020-07-26 16:53:09 -0700

    Create About container, row, and columns

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function About() {
   return(
     <Container fluid className="About-container">
-      <img src="https://randomuser.me/api/portraits/lego/5.jpg" />
-      <p>
-        Tristique senectus et netus et. Suspendisse sed nisi lacus sed viverra tellus in hac. Purus in massa tempor nec feugiat. Diam vulputate ut pharetra sit amet aliquam id. Blandit volutpat maecenas volutpat blandit aliquam etiam. Quis varius quam quisque id.
-      </p>
+      <Row className="About-row">
+        <Col className="About-col">
+          <Image roundedCircle src="https://randomuser.me/api/portraits/lego/5.jpg" className="About-img" /> 
+        </Col>
+        
+        <Col className="About-col">
+          <p>
+            Tristique senectus et netus et. Suspendisse sed nisi lacus sed viverra tellus in hac. Purus in massa tempor nec feugiat. Diam vulputate ut pharetra sit amet aliquam id. Blandit volutpat maecenas volutpat blandit aliquam etiam. Quis varius quam quisque id.
+          </p>
+        </Col>
+        
+      </Row>
+
+      
     </Container>
   )
 }
