bca4572081f7d8f4d159103cd055e9827f5e9d65
gcmartin <gscalica@gmail.com>
2020-07-26 17:49:44 -0700

    Position About text

--- src/components/About.js
+++ src/components/About.js
@@ -8,16 +8,19 @@ function About() {
   return(
     <Container fluid className="About-container">
       <Row className="About-row">
+
         <Col className="About-col-img">
           <Image roundedCircle src="https://randomuser.me/api/portraits/lego/5.jpg" className="About-img" /> 
         </Col>
         
-        <Col className="About-col">
-          <p>
-            Tristique senectus et netus et. Suspendisse sed nisi lacus sed viverra tellus in hac. Purus in massa tempor nec feugiat. Diam vulputate ut pharetra sit amet aliquam id. Blandit volutpat maecenas volutpat blandit aliquam etiam. Quis varius quam quisque id.
-          </p>
-        </Col>
-        
+        <Col className="About-col-text">
+            <p className="About-col-p">
+              Tristique senectus et netus et. Suspendisse sed nisi lacus sed viverra tellus in hac. Purus in massa tempor nec feugiat. Diam vulputate ut pharetra sit amet aliquam id. Blandit volutpat maecenas volutpat blandit aliquam etiam. Quis varius quam quisque id.
+              <br />
+              <br />
+              Tristique senectus et netus et. Suspendisse sed nisi lacus sed viverra tellus in hac. Purus in massa tempor nec feugiat. Diam vulputate ut pharetra sit amet aliquam id. Blandit volutpat maecenas volutpat blandit aliquam etiam. Quis varius quam quisque id.
+            </p>  
+        </Col> 
       </Row>
 
       
