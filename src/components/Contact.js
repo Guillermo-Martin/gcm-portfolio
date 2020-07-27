9a9f52628f5518288bbb71a8c47a9ddafb615d3a
gcmartin <gscalica@gmail.com>
2020-07-27 13:56:25 -0700

    Add Bootstrap container with content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 function Contact() {
   return(
-    <div>
-      <h1>This is the Contact section.</h1>
-    </div>
+    <Container className="Contact-container">
+      <div>
+        <h1>This is the Contact section.</h1>
+        <p>Diam vulputate ut pharetra sit amet aliquam id.</p>
+        <p>Icons will go here.</p>
+      </div>
+    </Container>
+    
   )
 }
 
