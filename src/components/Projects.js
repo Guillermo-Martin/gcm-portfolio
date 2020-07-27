129066990ca5eba6c466f23e7d070c5f7db9a1dd
gcmartin <gscalica@gmail.com>
2020-07-27 10:46:52 -0700

    Create container and row in Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Projects() {
   return(
     <div className="Projects-container">
-      <p>This is the projects section.</p>
+      <h1>This is the projects section.</h1>
+      <p>
+        Quam id leo in vitae turpis massa. Convallis aenean et tortor at risus viverra adipiscing. Sit amet justo donec enim. Arcu felis bibendum ut tristique. Quis lectus nulla at volutpat diam ut venenatis.
+      </p>
+      <Container fluid>
+        <Row className="Projects-row">
+          <p>This is a row.</p>
+        </Row>
+      </Container>
     </div>
   );
 }
