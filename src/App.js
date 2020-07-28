53ecc019ca74eb3bea14a07ca79a3c6bb4b73190
gcmartin <gscalica@gmail.com>
2020-07-27 17:00:53 -0700

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroImage from './components/HeroImage';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
