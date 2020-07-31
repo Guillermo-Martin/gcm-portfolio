1b59bb81abfd1b7c610816065407f2f76b06d7a4
gcmartin <gscalica@gmail.com>
2020-07-30 17:43:01 -0700

    Clean up code and add comments in the About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function About() {
-  return(
-
+  return (
     <div className="About-container">
-      <h1 className="About-header">This is the about section.</h1>
-      <p className="Projects-text">
+
+      {/* Header and text */}
+      <p className="About-header">This is the about section.</p>
+      <p>
         Quam id leo in vitae turpis massa. Convallis aenean et tortor at risus viverra adipiscing. Sit amet justo donec enim. Arcu felis bibendum ut tristique. Quis lectus nulla at volutpat diam ut venenatis.
       </p>
-      {/* <Container> */}
-      <Image roundedCircle src="https://randomuser.me/api/portraits/lego/5.jpg" /> 
-      {/* </Container>   */}
+
+      {/* Image */}
+      <Image roundedCircle src="https://randomuser.me/api/portraits/lego/5.jpg" />   
     </div>
-  )
+  );
 }
 
 export default About;
