0238f21f01cdb82514f57bd753a860b4f43a2758
gcmartin <gscalica@gmail.com>
2020-07-30 17:46:00 -0700

    Clean up code and add comments in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,17 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
 function Contact() {
-  return(
+  return (
     <Container className="Contact-container">
       <div>
+
+        {/* Header and text */}
         <h2 className="Contact-header">This is the Contact section.</h2>
         <p>Diam vulputate ut pharetra sit amet aliquam id.</p>
+
+        {/* Button */}
         <Button>Button</Button>
+
+        {/* Icons */}
         <p className="Contact-icons">Icons will go here.</p>
+
       </div>
-    </Container>
-    
-  )
+    </Container>  
+  );
 }
 
 export default Contact;
