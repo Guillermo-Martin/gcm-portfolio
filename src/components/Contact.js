908a27c12254492796e6db50eacf29d85db58296
gcmartin <gscalica@gmail.com>
2020-08-02 13:53:37 -0700

    Add Font Awesome CDN and icons in Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
 
 function Contact() {
   return (
     <Container className="Contact-container">
       <div>
 
-        {/* Header and text */}
-        <h2 className="Contact-header">Say Hello.</h2>
-        <p>Reach out and say hi or connect with me.  I look forward to hearing from you.</p>
+          {/* Header and text */}
+          <h2 className="Contact-header">Say Hello.</h2>
+          <p>Reach out and say hi or connect with me.  I look forward to hearing from you.</p>
 
-        {/* Button */}
-        <Button>Email Me.</Button>
-
-        {/* Icons */}
-        {/* ICONS WILL GO HERE */}
-
+          {/* Button */}
+          <Button><i class="far fa-envelope"></i>Email Me.</Button>
+        
+        <Row>
+          {/* Icons */}
+          <i class="fab fa-linkedin"></i>
+          <i class="fab fa-github"></i>
+        </Row>
+        
       </div>
     </Container>  
   );
