4b7e9bca5503fad46a27d88c6ea713965a3c0148
gcmartin <gscalica@gmail.com>
2020-08-02 14:15:45 -0700

    Add links to Font Awesome icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,17 +8,17 @@ function Contact() {
     <Container className="Contact-container">
       <div>
 
-          {/* Header and text */}
-          <h2 className="Contact-header">Say Hello.</h2>
-          <p>Reach out and say hi or connect with me.  I look forward to hearing from you.</p>
+        {/* Header and text */}
+        <h2 className="Contact-header">Say Hello.</h2>
+        <p>Reach out and say hi or connect with me.  I look forward to hearing from you.</p>
 
-          {/* Button */}
-          <Button><i className="far fa-envelope"></i>Email Me.</Button>
+        {/* Button */}
+        <a href="mailto:gscalica@gmail.com"><Button><i className="far fa-envelope"></i>Email Me.</Button></a>
         
+        {/* Icons */}
         <Row className="Contact-row-icons">
-          {/* Icons */}
-          <i className="fab fa-linkedin icon"></i>
-          <i className="fab fa-github icon"></i>
+          <a href="https://www.linkedin.com/in/guillermo-martin-10441046/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin icon"></i></a>
+          <a href="https://github.com/Guillermo-Martin" target="_blank" rel="noopener noreferrer"><i className="fab fa-github icon"></i></a>
         </Row>
         
       </div>
