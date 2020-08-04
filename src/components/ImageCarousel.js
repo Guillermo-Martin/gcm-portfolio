5355b32b2643df34b6ed4fc6583f78ccb7906880
gcmartin <gscalica@gmail.com>
2020-08-03 17:30:58 -0700

    Re-style ImageCarousel component

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container'
+import blog from './../assets/images/blogSite.PNG';
 
 function ImageCarousel() {
   return (
-    <div>
+    // <div>
 
-      <Container fluid className="ImageCarousel-container">
+      <Container className="ImageCarousel-container">
 
         {/* Carousel */}
         <Carousel interval={null}>
 
           {/* Carousel item */}
-          <Carousel.Item>
+          <Carousel.Item className="ImageCarousel-item">
             <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1526436177729-efd2fa1172a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2304&q=80"
+              className="ImageCarousel-img"
+              src={blog}
+              // src="https://images.unsplash.com/photo-1526436177729-efd2fa1172a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2304&q=80"
               alt="First slide"
             />
+            
             {/* Carousel caption */}
             <Carousel.Caption>
               <h3>First slide label</h3>
@@ -26,7 +29,7 @@ function ImageCarousel() {
           </Carousel.Item>
 
           {/* Carousel item */}
-          <Carousel.Item className="Carousel-item">
+          <Carousel.Item className="ImageCarousel-item">
             <img
               className="d-block w-100"
               src="https://images.unsplash.com/photo-1526436177729-efd2fa1172a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2304&q=80"
@@ -40,7 +43,7 @@ function ImageCarousel() {
           </Carousel.Item>
 
           {/* Carousel item */}
-          <Carousel.Item className="Carousel-item">
+          <Carousel.Item className="ImageCarousel-item">
             <img
               className="d-block w-100"
               src="https://images.unsplash.com/photo-1526436177729-efd2fa1172a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2304&q=80"
@@ -55,7 +58,7 @@ function ImageCarousel() {
         </Carousel>
       </Container>
 
-    </div>
+    // </div>
   );
 }
 
