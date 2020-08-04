ab2c445544cc5402d3037bbbae5ac87de9673870
gcmartin <gscalica@gmail.com>
2020-08-04 14:21:46 -0700

    Add darkening effect on hover for ImageCard image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -7,7 +7,8 @@ function ImageCard() {
       <Card style={{ width: '30rem', margin: "0.5rem" }}>
         <Card.Img 
           variant="top" 
-          src="https://images.unsplash.com/photo-1511017049469-e0d1ba0219a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80" 
+          src="https://images.unsplash.com/photo-1511017049469-e0d1ba0219a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
+          className="hello" 
           />
           {/* <Card.Title className="text">Card Title</Card.Title> */}
           <div className="ImageCard-overlay">
