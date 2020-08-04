92602bb23cbf4e49ad53dc788cf550ea7053ff80
gcmartin <gscalica@gmail.com>
2020-08-04 15:24:08 -0700

    Revise ImageCard component hover effect

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,15 +3,14 @@ import Card from 'react-bootstrap/Card';
 
 function ImageCard() {
   return (
-    <div className="ImageCard-div">
+    <div className="ImageCard-container">
       <Card style={{ width: '30rem', margin: "0.5rem" }}>
         <Card.Img 
           variant="top" 
           src="https://images.unsplash.com/photo-1511017049469-e0d1ba0219a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
-          className="hello" 
           />
-          {/* <Card.Title className="text">Card Title</Card.Title> */}
           <div className="ImageCard-overlay">
+            {/* <Card.Title className="text">Card Title</Card.Title> */}
             <Card.Text className="ImageCard-text">
               Some quick example text to build on the card title and make up the bulk of
               the card's content.
