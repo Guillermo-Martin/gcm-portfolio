c7ec273c0aed0d4b12fb9e64c03456db91d9140f
gcmartin <gscalica@gmail.com>
2020-08-06 16:25:35 -0700

    Replace ProjectCard with ImageCard, delete ProjectCard component, and center ImageCards in Porfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProjectCard from './ProjectCard';
+import ImageCard from './ImageCard';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
@@ -16,34 +16,34 @@ function Portfolio() {
       <p>
         Here's a selection of work I've done.
       </p>
-      <Container>
+      <Container fluid>
 
         {/* Project cards */}
         <Row className="Portfolio-row">
-          <ProjectCard
-            src={passwordGenerator}
+        <ImageCard
+            src={passwordGenerator} 
             title="Password Generator"
             description="A web application that uses prompts to generate a random password."
             tech="Made using: HTML, CSS, JavaScript, and GitHub Pages."
+            link="https://guillermo-martin.github.io/passwordGenerator/"
             github="https://github.com/Guillermo-Martin/passwordGenerator"
-            livelink="https://guillermo-martin.github.io/passwordGenerator/"
           />
-          <ProjectCard
-            src={weatherDashboard}
+          <ImageCard
+            src={weatherDashboard} 
             title="Weather Dashboard"
             description="A web application that provides the current weather and a 5-day forecast for a city."
-            tech="Made using: HTML, CSS, JavaScript, jQuery, Bootstrap, OpenWeather API, Moment.js, and GitHub Pages."
+            tech="HTML, CSS, JavaScript, jQuery, Bootstrap, OpenWeather API, Moment.js, and GitHub Pages."
+            link="https://guillermo-martin.github.io/weatherDashboard/"
             github="https://github.com/Guillermo-Martin/weatherDashboard"
-            livelink="https://guillermo-martin.github.io/weatherDashboard/"
           />
-          <ProjectCard
-            src={blogSite}
+          <ImageCard
+            src={blogSite} 
             title="Blog Site"
-            description="A blog site that allows users to post, view recipes, or Pokemon information."
-            tech="Made using: HTML, CSS, JavaScript, React, Bootstrap, TravisCI, ESLint, Express, mySQL2, Node.js, and Heroku."
+            description="A web application for posting thoughts, learning new recipes, and facts about Pokemon."
+            tech="HTML, CSS, JavaScript, React, Bootstrap, TravisCI, ESLint, Express, mySQL2, Node.js, and Heroku."
+            link="https://project02blog.herokuapp.com/"
             github="https://github.com/Kerry-Jr/Project02"
-            livelink="https://project02blog.herokuapp.com/"
-          />
+        />
         </Row>
         
         {/* Link to see more */}
