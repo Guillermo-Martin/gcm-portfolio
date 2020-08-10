ad12bb99e4ccfee81ba22d91da1294553445e349
gcmartin <gscalica@gmail.com>
2020-08-10 11:46:35 -0700

    Fix About link in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -17,7 +17,7 @@ function Navigation() {
           {/* Links */}
           <Navbar.Collapse className="justify-content-end">
               <Nav.Item>
-                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/#about">About</Nav.Link>
               </Nav.Item>
 
               <Nav.Item>
