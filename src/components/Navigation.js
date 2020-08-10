4b52f7e716c88a53ed4210b468ce8f7463552b5e
gcmartin <gscalica@gmail.com>
2020-08-10 13:38:47 -0700

    Style Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,19 +9,19 @@ function Navigation() {
     <div>
       <Container fluid>
 
-        <Navbar expand="lg" fixed="sticky">
-
-          <Navbar.Brand><Link to='/'>Guillermo Martin</Link></Navbar.Brand>
+        <Navbar expand="lg" fixed="top" className="Navbar">
+        
+          <Navbar.Brand><Link to='/' className="Navbar-link">Guillermo Martin</Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
 
           {/* Links */}
           <Navbar.Collapse className="justify-content-end">
               <Nav.Item>
-                <Nav.Link href="/#about">About</Nav.Link>
+                <Nav.Link href="/#about" className="Navbar-link">About</Nav.Link>
               </Nav.Item>
 
               <Nav.Item>
-                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="/portfolio" className="Navbar-link">Portfolio</Nav.Link>
               </Nav.Item>
           </Navbar.Collapse>
         </Navbar>
