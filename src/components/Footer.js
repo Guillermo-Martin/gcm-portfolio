dae7e504660ea103922a1dbdc8cb2d136cbb83b6
gcmartin <gscalica@gmail.com>
2020-08-12 11:13:39 -0700

    Change Footer component to Bootstrap Nav and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 function Footer() {
+  
   return (
-    <div className="Footer">
-      <p>Arcu felis bibendum ut tristique.</p>
-    </div>
+    <Nav className="justify-content-center Footer">
+      
+      <Nav.Item>
+        <Nav.Link className="Footer-copyright" href="/">&copy; 2020 Copyright Guillermo Martin.</Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link href="https://github.com/Guillermo-Martin" target="_blank" rel="noopener noreferrer"><i className="fab fa-github Footer-icon"></i></Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://www.linkedin.com/in/guillermo-martin-10441046/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin Footer-icon"></i></Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="mailto:gscalica@gmail.com"><i className="far fa-envelope Footer-icon"></i></Nav.Link>
+      </Nav.Item>
+        
+    </Nav>  
   );
 }
 
