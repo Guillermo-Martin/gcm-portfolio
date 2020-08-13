94f95ab1d628e7d96b1155114faffe80638d5650
gcmartin <gscalica@gmail.com>
2020-08-13 12:09:18 -0700

    Create ScrollToTop.js and implement render in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navigation from './components/Navigation';
 import HomePage from './components/HomePage';
 import PortfolioPage from './components/PortfolioPage';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Navigation />
         <Route exact path='/gcm-portfolio' component={HomePage} />
